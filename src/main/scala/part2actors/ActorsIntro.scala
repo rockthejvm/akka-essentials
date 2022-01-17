76a1e077254b7ee7b67a3e5fcaea6c41d4a24e9c
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2022-01-17 15:45:26 +0200

    actors exercises

--- src/main/scala/part2actors/ActorsIntro.scala
+++ src/main/scala/part2actors/ActorsIntro.scala
@@ -6,7 +6,7 @@ import akka.actor.typed.scaladsl.Behaviors
 object ActorsIntro {
 
   // part 1: behavior
-  val simpleActorBehavior: Behavior[String] = Behaviors.receiveMessage{ (message: String) =>
+  val simpleActorBehavior: Behavior[String] = Behaviors.receiveMessage { (message: String) =>
     // do something with the message
     println(s"[simple actor] I have received: $message")
 
@@ -16,7 +16,7 @@ object ActorsIntro {
 
   def demoSimpleActor(): Unit = {
     // part 2: instantiate
-    val actorSystem = ActorSystem(SimpleActor_V2(), "FirstActorSystem")
+    val actorSystem = ActorSystem(Person.happy(), "FirstActorSystem")
 
     // part 3: communicate!
     actorSystem ! "I am learning Akka" // asynchronously send a message
@@ -60,8 +60,104 @@ object ActorsIntro {
     }
   }
 
+  /**
+   * Exercises
+   * 1. Define two "person" actor behaviors, which receive Strings:
+   *  - "happy", which logs your message, e.g. "I've received ____. That's great!"
+   *  - "sad", .... "That sucks."
+   *  Test both.
+   *
+   * 2. Change the actor behavior:
+   *  - the happy behavior will turn to sad() if it receives "Akka is bad."
+   *  - the sad behavior will turn to happy() if it receives "Akka is awesome!"
+   *
+   * 3. Inspect my code and try to make it better.
+   */
+
+  object Person {
+    def happy(): Behavior[String] = Behaviors.receive { (context, message) =>
+      message match {
+        case "Akka is bad." =>
+          context.log.info("Don't you say anything bad about Akka!!!")
+          sad()
+        case _ =>
+          context.log.info(s"I've received '$message'.That's great!")
+          Behaviors.same
+      }
+    }
+
+    def sad(): Behavior[String] = Behaviors.receive { (context, message) =>
+      message match {
+        case "Akka is awesome!" =>
+          context.log.info("Happy now!")
+          happy()
+        case _ =>
+          context.log.info(s"I've received '$message'.That sucks!")
+          Behaviors.same
+      }
+    }
+
+    def apply(): Behavior[String] = happy()
+  }
+
+  def testPerson(): Unit = {
+    val person = ActorSystem(Person(), "PersonTest")
+
+    person ! "I love the color blue."
+    person ! "Akka is bad."
+    person ! "I also love the color red."
+    person ! "Akka is awesome!"
+    person ! "I love Akka."
+
+    Thread.sleep(1000)
+    person.terminate()
+  }
+
+  object WeirdActor {
+    // wants to receive messages of type Int AND String
+    def apply(): Behavior[Any] = Behaviors.receive { (context, message) =>
+      message match {
+        case number: Int =>
+          context.log.info(s"I've received an int: $number")
+          Behaviors.same
+        case string: String =>
+          context.log.info(s"I've received a String: $string")
+          Behaviors.same
+      }
+    }
+  }
+
+  // solution: add wrapper types & type hierarchy (case classes/objects)
+  object BetterActor {
+    trait Message
+    case class IntMessage(number: Int) extends Message
+    case class StringMessage(string: String) extends Message
+
+    def apply(): Behavior[Message] = Behaviors.receive { (context, message) =>
+      message match {
+        case IntMessage(number) =>
+          context.log.info(s"I've received an int: $number")
+          Behaviors.same
+        case StringMessage(string) =>
+          context.log.info(s"I've received a String: $string")
+          Behaviors.same
+      }
+    }
+  }
+
+
+  def demoWeirdActor(): Unit = {
+    import BetterActor._
+    val weirdActor = ActorSystem(BetterActor(), "WeirdActorDemo")
+    weirdActor ! IntMessage(43) // ok
+    weirdActor ! StringMessage("Akka") // ok
+    // weirdActor ! '\t' // not ok
+
+    Thread.sleep(1000)
+    weirdActor.terminate()
+  }
 
   def main(args: Array[String]): Unit = {
-    demoSimpleActor()
+    demoWeirdActor()
   }
 }
